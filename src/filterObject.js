69127ebef9fd3583e2c3c2d9c9420f7d8869f862
Nicolás López <nicolaslopezj@me.com>
2018-11-11 22:56:40 -0300

    filter object

---
+++ src/filterObject.js
@@ -0,0 +1,14 @@
+import isPlainObject from 'lodash/isPlainObject'
+import isArray from 'lodash/isArray'
+
+export default function filterObject(obj, key) {
+  for (var i in obj) {
+    if (!obj.hasOwnProperty(i)) continue
+    if (isPlainObject(obj[i]) || isArray(obj[i])) {
+      filterObject(obj[i], key)
+    } else if (i === key) {
+      delete obj[key]
+    }
+  }
+  return obj
+}
--- src/useMutation.js
+++ src/useMutation.js
@@ -1,16 +1,14 @@
-/**
- * This code is from
- * https://github.com/trojanowski/react-apollo-hooks/blob/master/src/index.js
- */
-
 import useClient from './useClient'
+import filterObject from './filterObject'
+import cloneDeep from 'lodash/cloneDeep'
 
 export default function useMutation(mutation, baseOptions) {
   const client = useClient()
   return async (variables, localOptions) => {
+    const finalVariables = filterObject(cloneDeep(variables), '__typename')
     const result = await client.mutate({
       mutation,
-      variables,
+      variables: finalVariables,
       ...baseOptions,
       ...localOptions
     })
