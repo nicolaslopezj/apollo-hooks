8f36cf5887dbcad70b7e6ccbb7a32bbc4cc387eb
Nicolás López <nicolaslopezj@me.com>
2018-11-11 22:27:44 -0300

    return data in mutation

--- src/useMutation.js
+++ src/useMutation.js
@@ -8,11 +8,16 @@ import useClient from './useClient'
 export default function useMutation(mutation, baseOptions) {
   const client = useClient()
   return (variables, localOptions) => {
-    return client.mutate({
+    const result = client.mutate({
       mutation,
       variables,
       ...baseOptions,
       ...localOptions
     })
+
+    return {
+      ...result.data,
+      ...result
+    }
   }
 }
