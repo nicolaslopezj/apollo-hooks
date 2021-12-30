ac462072b991dacf2155812a1860c1f1b1375474
Nicolás López Jullian <nicolaslopezj@me.com>
2021-12-30 12:13:20 -0300

    type use mutate

--- src/useMutate.ts
+++ src/useMutate.ts
@@ -2,14 +2,20 @@ import filterObject from './filterObject'
 import cloneDeep from 'lodash/cloneDeep'
 import useClients from './useClients'
 import getClient from './getClient'
+import {MutationOptions} from '@apollo/client'
+import {omit} from 'lodash'
+
+export type UseMutateOptions<TData, TVariables> = MutationOptions<TData, TVariables, any> & {
+  clientName: string
+}
 
 export default function useMutate() {
   const clients = useClients()
-  return async options => {
+  return async <TData, TVariables>(options: UseMutateOptions<TData, TVariables>) => {
     const finalVariables = filterObject(cloneDeep(options.variables || {}), '__typename')
     const client = getClient(clients, options.clientName)
-    const result = await client.mutate({
-      ...options,
+    const result = await client.mutate<TData, TVariables>({
+      ...omit(options, 'clientName'),
       variables: finalVariables
     })
     return result.data
