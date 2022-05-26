23b41e54d6c29946e249a34b9b15b5c6795d9698
Nicolás López <nicolaslopezj@me.com>
2022-05-26 12:46:00 -0400

    new way of compiling

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const {defineConfig} = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'apollo-hooks',
+      fileName: format => `apollo-hooks.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  }
+})
