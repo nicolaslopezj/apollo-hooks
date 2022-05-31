16e599a822144700915fcb92e2b6d396de134ee2
Nicolás López <nicolaslopezj@me.com>
2022-05-30 23:51:36 -0400

    type use refetch queries

--- src/useRefetchQueries.ts
+++ src/useRefetchQueries.ts
@@ -1,21 +1,25 @@
 import useClient from './useClient'
 import isArray from 'lodash/isArray'
 
-export default function useRefetchQueries(options: any = {}) {
+export interface RefetchQueriesOptions {
+  clientName?: string
+}
+
+export default function useRefetchQueries(options: RefetchQueriesOptions = {}) {
   const client = useClient(options.clientName) as any
 
-  return async names => {
-    names = isArray(names) ? names : [names]
+  return async (queryNames: string | string[]) => {
+    queryNames = isArray(queryNames) ? queryNames : [queryNames]
     const refetchQueryPromises = []
 
     const qManager = client.queryManager
     qManager.queries.forEach(({observableQuery}) => {
-      if (observableQuery && names.includes(observableQuery.queryName)) {
+      if (observableQuery && queryNames.includes(observableQuery.queryName)) {
         refetchQueryPromises.push(observableQuery.refetch())
       }
     })
 
     const results = await Promise.all(refetchQueryPromises)
-    return isArray(names) ? results : results[0]
+    return isArray(queryNames) ? results : results[0]
   }
 }
