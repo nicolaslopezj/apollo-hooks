515581e70810ab0569f242c270eb1df6087e76f4
Nicolás López <nicolaslopezj@me.com>
2022-12-05 13:55:12 -0300

    type use client

--- src/useClient.ts
+++ src/useClient.ts
@@ -2,7 +2,7 @@ import ApolloContext from './ApolloContext'
 import {useContext} from 'react'
 import getClient from './getClient'
 
-export default function useClient(clientName) {
+export default function useClient(clientName?: string) {
   const clients = useContext(ApolloContext)
   return getClient(clients, clientName)
 }
